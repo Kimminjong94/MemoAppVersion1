ca82eae58d8c640e5d6e655b580dab58c08eaff7
minjongkim <rlaalswhd200@naver.com>
2021-10-21 13:44:57 +0900

    xcode 연동커밋

---
+++ MemoAppVersion1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoAppVersion1
+//
+//  Created by 김민종 on 2021/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// doing changes
+}
+
